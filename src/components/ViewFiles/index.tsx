5ecc65bcc04b851c8f6f7cc690d6f944267ff78c
Jamal Ahmed <j8ahmed@gmail.com>
2023-09-16 14:02:11 -0400

    Add generic icon for non-image file types

--- src/components/ViewFiles/index.tsx
+++ src/components/ViewFiles/index.tsx
@@ -2,9 +2,22 @@ import { useState } from "react"
 import useFiles from "@/hooks/useFiles"
 import styles from "./style.module.scss";
 
+
 export default function ViewFiles() {
     const fileList = useFiles();
-    console.log(fileList)
+
+    const fileIsImage = (url: string) => {
+        const imageFileTypes = ["png", "jpg", "jpeg", "gif", "svg"]
+
+        const decodedURI = decodeURIComponent(url)
+        const regex = new RegExp("files\/[a-z0-9-_=]+\.\([a-z]+\)", "i")
+        const matches = decodedURI.match(regex)
+        if (matches){
+            const fileType = matches[1]
+            return imageFileTypes.includes(fileType)
+        }
+        return false
+    }
 
     return (
         <div id={styles.container}>
@@ -12,9 +25,14 @@ export default function ViewFiles() {
                 {fileList.map((file, i) => {
                     const {name, imageLink} = file;
                     return (
-                        <a class={styles.fileLink} href={imageLink} rel="noreferrer noopener" target="_blank">
-                            <li key={i} className={styles.file}>
-                                <img className={styles.img} src={imageLink} alt={name} title={name} />
+                        <a key={i} className={styles.fileLink} href={imageLink} rel="noreferrer noopener" target="_blank">
+                            <li className={styles.file}>
+                                { fileIsImage(imageLink) ? 
+                                    <img className={styles.img} src={imageLink} alt={name} title={name} /> :
+                                    <svg className={styles.svg} alt={name} title={name} xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512">
+                                        <path d="M320 464c8.8 0 16-7.2 16-16V160H256c-17.7 0-32-14.3-32-32V48H64c-8.8 0-16 7.2-16 16V448c0 8.8 7.2 16 16 16H320zM0 64C0 28.7 28.7 0 64 0H229.5c17 0 33.3 6.7 45.3 18.7l90.5 90.5c12 12 18.7 28.3 18.7 45.3V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64z"/>
+                                    </svg>
+                                }
                                 <h4 className={styles.fileHeading}>{name.length > 15 ? name.substring(0, 15) + "..." : name}</h4>
                             </li>
                         </a>
