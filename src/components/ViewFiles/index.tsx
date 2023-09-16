bb4a94eebe83e86fc7f27c74f8349029bfe7b675
Jamal Ahmed <j8ahmed@gmail.com>
2023-09-16 14:09:04 -0400

    Add title tag to svg on mouseover

--- src/components/ViewFiles/index.tsx
+++ src/components/ViewFiles/index.tsx
@@ -6,12 +6,11 @@ import styles from "./style.module.scss";
 export default function ViewFiles() {
     const fileList = useFiles();
 
-    const fileIsImage = (url: string) => {
+    const fileIsImage = (filename: string) => {
         const imageFileTypes = ["png", "jpg", "jpeg", "gif", "svg"]
 
-        const decodedURI = decodeURIComponent(url)
-        const regex = new RegExp("files\/[a-z0-9-_=]+\.\([a-z]+\)", "i")
-        const matches = decodedURI.match(regex)
+        const regex = new RegExp("\.\([a-z]+\)$", "i")
+        const matches = filename.match(regex)
         if (matches){
             const fileType = matches[1]
             return imageFileTypes.includes(fileType)
@@ -27,9 +26,10 @@ export default function ViewFiles() {
                     return (
                         <a key={i} className={styles.fileLink} href={imageLink} rel="noreferrer noopener" target="_blank">
                             <li className={styles.file}>
-                                { fileIsImage(imageLink) ? 
+                                { fileIsImage(name) ? 
                                     <img className={styles.img} src={imageLink} alt={name} title={name} /> :
-                                    <svg className={styles.svg} alt={name} title={name} xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512">
+                                    <svg className={styles.svg} alt={name} xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512">
+                                        <title>{name}</title>
                                         <path d="M320 464c8.8 0 16-7.2 16-16V160H256c-17.7 0-32-14.3-32-32V48H64c-8.8 0-16 7.2-16 16V448c0 8.8 7.2 16 16 16H320zM0 64C0 28.7 28.7 0 64 0H229.5c17 0 33.3 6.7 45.3 18.7l90.5 90.5c12 12 18.7 28.3 18.7 45.3V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64z"/>
                                     </svg>
                                 }
